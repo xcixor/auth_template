74b7c414e9f0774c7233f47b22e7e1cbc7708b8e
Peter <pndungu54@gmail.com>
2017-10-08 19:14:29 +0300

    added the auth blueprint constructor

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,2 +1,11 @@
+"""This is the auth blueprint constructor.
+It creates the blueprint object and imports routes from views.py module.
+"""
 from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+from . import views
+
+
 
