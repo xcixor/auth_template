4d98727c24795bb644c5faf12a5311b0c9903353
Peter <pndungu54@gmail.com>
2017-10-10 01:52:27 +0300

    organized the imports so that the auth blueprint is imported after the login manager has been instantiated to remove errors. should watch out in future

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ It also imports other objects used by the application
 It defines a method called 'create_app' which serves as the application factory.
 This method takes as an argument, the name of the configuration to use for the app.
 """
-from flask import Flask, render_template
+from flask import Flask
 
 from flask_bootstrap import Bootstrap
 
@@ -14,11 +14,11 @@ from flask_moment import Moment
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_login import LoginManager
+
 from config import config
 
-from .auth import auth as auth_blueprint
-
-from flask_login import LoginManager
+# from .main import main as main_blueprint
 
 """Creating the extensions objects"""
 
@@ -29,6 +29,7 @@ mail = Mail()
 moment = Moment()
 
 db = SQLAlchemy()
+
 
 login_manager = LoginManager()
 
@@ -47,15 +48,16 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    #BluePrints
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
     #Extensions
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     mail.init_app(app)
     login_manager.init_app(app)
+
+    #BluePrints
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     #routes and custom errors go here
 
