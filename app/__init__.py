926037885176abfd74a089a53c991e1a819059eb
Peter <pndungu54@gmail.com>
2017-10-09 12:26:39 +0300

    registered the auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
+from .auth import auth as auth_blueprint
+
 """Creating the extensions objects"""
 
 bootstrap = Bootstrap()
@@ -36,6 +38,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    #BluePrints
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     bootstrap.init_app(app)
     moment.init_app(app)
