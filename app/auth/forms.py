b619838eb3832c3426d35e7f02a94859c77f7fc4
Peter <pndungu54@gmail.com>
2017-10-09 18:49:35 +0300

    added the login functionality

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -24,3 +24,4 @@ class LoginForm(Form):
 
 
 
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,27 @@
 """This module imports the auth blueprint.
 It also defines the routes associated with user authenication.
 """
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+
+from flask_login import login_user
 
 from . import auth
 
-@auth.route('/login')
+from ..models import User
+
+from .forms import LoginForm
+
+from . import auth
+
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    """Logs the user in"""
-    return render_template('auth/login.html')
+    """Validates the User details and tries to Log the user in"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('/main.index'))
+        flash('invalid username or password')
+    return render_template('auth/login.html', form=form)
+
