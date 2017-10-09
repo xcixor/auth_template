b218b70fb2fc1576dde1e948f38db4561e9414ff
Peter <pndungu54@gmail.com>
2017-10-10 01:53:29 +0300

    reorganized the imports

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,19 +3,19 @@ It also defines the routes associated with user authenication.
 """
 from flask import render_template, redirect, request, url_for, flash
 
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 from . import auth
 
 from ..models import User
 
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 from . import auth
 
 from .. import db
 
-from .forms import User, RegistrationForm
+from ..emails import send_email
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
