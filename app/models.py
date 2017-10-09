b2d5958909e236920e6e541a023e701d365c9686
Peter <pndungu54@gmail.com>
2017-10-09 12:53:23 +0300

    added the User model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+"""Defines the Database Models of the application
+Classes(Models):
+  User: Represents a user in the database
+  Role: Defines the role of a user in the application
+"""
 
-# from flask.ext.bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+from flask_login import UserMixin
 
-app = Flask(__name__)      
+class User(db.Model):
+      """Instantiates a User object that can be stored in the db
+      Attributes:
+        __tablename__(string): Name of the table in the db
+        id(Integer): A unique key that represents a particular user
+        username(String): The username of the user
+        role_id(Integer): Represents a foreign key to the Role table establishing one-many relationship
+        email(String): Represents the user's email
+      Methods:
+        __repr__: Returns a string representation a string representation of the class
+      """
+      __tablename__ = 'users'
+      id = db.Column(db.Integer, primary_key=True)
+      email = db.Column(db.String(64), unique=True, index=True)
+      password_hash = db.Column(db.String(128))
+      role_id = db.Column(db.Intefer, db.ForeignKey('roles.id'))
 
-@app.route('/')
-def home():
-  return render_template('index.html')
- 
-if __name__ == '__main__':
-  app.run(debug=True)
+class Role(db.Model):
+      """"Creates the Roles defined for the users of the application
+      Attributes:
+        users(object): Represents the users associated with a certain role
+      """
+      pass
+      
