139dcb2b4f1660031876f6a4d29c0671d297d6f6
Peter <pndungu54@gmail.com>
2017-10-09 23:35:44 +0300

    removed db importation error from the auth views

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,6 +13,10 @@ from .forms import LoginForm
 
 from . import auth
 
+from .. import db
+
+from .forms import User, RegistrationForm
+
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
     """Validates the User details and tries to Log the user in"""
@@ -32,5 +36,15 @@ def logout():
     logout_user()
     flash('You have logged out')
     redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 
