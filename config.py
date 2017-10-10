550bae4795451e3e9eb5b4c25cfd26df1a8bee53
Peter <pndungu54@gmail.com>
2017-10-10 16:14:41 +0300

    random edits

--- config.py
+++ config.py
@@ -22,8 +22,14 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Your Cross Site Forgery Attempts are Futile'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[My_App]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <pndungu54@gmail.com.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <pndungu54@gmail.com.com>'
+    FLASKY_MAIL_SENDER = 'pndungu54@gmail.com.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'pndungu54@gmail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'joe'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'wN75216895#'
 
     @staticmethod
     def init_app(app):
@@ -47,11 +53,6 @@ class DevelopmentConfig(Config):
         _
     """
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
@@ -72,12 +73,19 @@ class ProductionConfig(Config):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
-config = {
-    """A dictionary registering the different configurations"""
+# config = {
+#     # """A dictionary registering the different configurations"""
     
+#     'development': DevelopmentConfig,
+#     'testing': TestingConfig,
+#     'production': ProductionConfig,
+#     'default': DevelopmentConfig
+# }
+
+config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+
     'default': DevelopmentConfig
 }
-
