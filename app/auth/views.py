121737472a115afb2fd3b3b1a06b7750ce8a2409
Peter <pndungu54@gmail.com>
2017-10-10 18:59:53 +0300

    edits

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,11 +22,11 @@ def login():
     """Validates the User details and tries to Log the user in"""
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first
+        user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('/main.index'))
-        flash('invalid username or password')
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
 @auth.route('/logout')
@@ -35,7 +35,7 @@ def logout():
     """Removes and resets the user session"""
     logout_user()
     flash('You have logged out')
-    redirect(url_for('main.index'))
+    return redirect(url_for('main.index'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import login_manager, db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """Instantiates a User object that can be stored in the db
     Attributes:
       __tablename__(string): Name of the table in the db
@@ -25,8 +25,23 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class Role(db.Model):
     """"Creates the Roles defined for the users of the application
