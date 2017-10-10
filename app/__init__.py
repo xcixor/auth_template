75fa3f71c764430ba43a7adfaa26149d3ddfbd45
Peter <pndungu54@gmail.com>
2017-10-10 15:05:31 +0300

    registered the main blueprint in the app

--- app/__init__.py
+++ app/__init__.py
@@ -56,6 +56,9 @@ def create_app(config_name):
     login_manager.init_app(app)
 
     #BluePrints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint, url_prefix='/main')
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
